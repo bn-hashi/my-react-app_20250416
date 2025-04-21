69ebb38374541a9561f9f0f17852cfc897e60c76
Kenji <bn.hachigo@gmail.com>
2025-04-21 23:27:21 +0900

    JSXの利用_JSXExample_20250421

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Counter from './components/Counter';
+import JSXExample from './components/JSXExample';
 
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Counter />
+    <JSXExample />
   </React.StrictMode>
 );
